970b8216f4dd563da00ef0d1a5f14820868c78ef
mydmk7874 <mydmk7874@gmail.com>
2023-01-16 23:08:47 +0900

    crawling.py를 app.py에 연결(app.py로 실행해도 작동하는것 확인함)

--- crawling.py
+++ crawling.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient
+from db import db
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.zfuzrjp.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route("/movies", methods=["POST"])
 def movies_post():
     url_receive = request.form['url_give']
     headers = {
@@ -49,12 +38,3 @@ def movies_post():
 
     return jsonify({'msg': '저장 완료!'})
 
-
-@app.route("/movies", methods=["GET"])
-def movie_get():
-    movie_list = list(db.movies.find({}, {'_id': False}))
-    return jsonify({'movies': movie_list})
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
