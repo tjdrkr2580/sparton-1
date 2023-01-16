685e09987bc687b9ed75498d73154ed3f6d29b23
mydmk7874 <mydmk7874@gmail.com>
2023-01-16 22:12:32 +0900

    app.py 조금 수정, movielist 수정

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from movielist import movie_list, add_movie
+from crawling import movies_post
+from comment import comment_get
+from
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,3 +14,26 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# 영화 목록 전체 조회 api
+@app.route('/movies', methods=["GET"])
+def movie_list_api():
+  msg = movie_list()
+  return msg
+
+# 영화 추가 api
+@app.route('/movies', methods=["POST"])
+def movies_post_api():
+  msg = movies_post()
+  return msg
+
+# 회원가입 api
+@app.route('/api/sign_up', methods=["POST"])
+def sign_up_api():
+  msg = sign_up()
+  return msg
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- movielist.py
+++ movielist.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from db import db
-
+from bs4 import BeautifulSoup
 
 #영화 리스트 불러오기
 def movie_list():
@@ -9,15 +9,27 @@ def movie_list():
 
 #리스트에 영화 추가하기
 def add_movie():
-    m_id_receive = request.form['m_id_give']
-    m_star_receive = request.form['m_star_give']
-    m_link_receive = request.form['m_link_give']
+    url_receive = request.form['url_give']
+
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get(url_receive, headers=headers)
+    #
+    # soup = BeautifulSoup(data.text, 'html.parser')
+    #
+    # title = soup.select_one('meta[property="og:title"]')['content']
+    # image = soup.select_one('meta[property="og:image"]')['content']
+    # desc = soup.select_one('meta[property="og:description"]')['content']
+
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    count = len(movie_list) + 1
 
     doc = {
-        'm_id' = m_id_receive,
-        'm_star' = m_star_receive,
-        'm_link' = m_link_receive
+        'm_id': count,
+        'm_star': m_star_receive,
+        'm_link': m_link_receive
     }
 
     db.movies.insert_one(doc)
 
+    return jsonify({'msg': '등록 완료!'})
