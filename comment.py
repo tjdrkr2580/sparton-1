fe5d343d7aab02b93172a8d852d065a2202f60a2
regular_kim <regular.jk.kim@gmail.com>
2023-01-16 23:04:17 +0900

    regular comments complete

--- comment.py
+++ comment.py
@@ -1,2 +1,29 @@
-from flask import jsonify, request
+from flask import jsonify, render_template, request
 from db import db
+
+
+@app.route("/detail")
+def movie_detail():
+    return render_template("movie_detail.html")
+
+
+@app.route("/comments", method=["GET"])
+def comments_get():
+    comments_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'comments': comments_list})
+
+
+@app.route("/comments", methods=["POST"])
+def comment_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
+    num_receive = request.form['m_id_give']
+
+    doc = {
+        'm_id': num_receive,
+        'c_id': name_receive,
+        'comment': comment_receive
+    }
+
+    db.comments.insert_one(doc)
+    return jsonify({'msg': '댓글 완료!'})
