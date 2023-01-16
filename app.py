3cf2ad696f844574c6809acaa0dedd5a84a3c6c6
regular_kim <regular.jk.kim@gmail.com>
2023-01-16 15:01:29 +0900

    common codes added

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("jk test")
+from flask import Flask, render_template, request, jsonify
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
